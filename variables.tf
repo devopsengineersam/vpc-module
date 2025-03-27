23c68d2bed9aa8041ec824fb481a833ada49ce78
sKisiangani <80091429+devopsengineersam@users.noreply.github.com>
2025-03-27 16:47:15 -0500

    added iam policy

--- variables.tf
+++ variables.tf
@@ -2,7 +2,6 @@ variable "resource_tags" {
   description = "Mandatory tags for all resources"
   type        = map(string)
   default = {
-    Environment = "POC"
     Owner       = "EIS"
     Project     = "prisma-dspm"
     ManagedBy   = "Terraform"
